c76b61458331676e4c74d37f111f0221084555d1
gs-ppara <ppara@gainsight.com>
2018-04-01 20:08:16 +0530

    Key Prop, Prop Types, spread operator examples added

---
+++ js/ShowCard.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { string } from "prop-types";
+
+const ShowCard = props => (
+  <div className="show-card">
+    <img
+      alt={`${props.title} Show Poster`}
+      src={`/public/img/posters/${props.poster}`}
+    />
+    <div>
+      <h3>{props.title}</h3>
+      <h4>({props.year})</h4>
+      <p>{props.description}</p>
+      {/* <h1>{props.foo}</h1> */}
+    </div>
+  </div>
+);
+
+// ShowCard.defaultProps = {
+//   foo: "stuff"
+// };
+
+// shape just means an object
+ShowCard.propTypes = {
+  poster: string.isRequired,
+  title: string.isRequired,
+  year: string.isRequired,
+  description: string.isRequired
+};
+
+export default ShowCard;
